60530f9c86e87fb8ea9b5152af305056020d51ba
Dmitrii Rybalko <rybalko02@gmail.com>
2020-11-09 10:53:15 +0300

    Логин закончен

--- Ваш БанкирЪ/MainMenuPage.xaml.cs
+++ Ваш БанкирЪ/MainMenuPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,11 +26,9 @@ namespace Ваш_БанкирЪ
         public MainMenuPage()
         {
             this.InitializeComponent();
+            
         }
 
-        private void backButton_Clicked(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(MainPage));
-        }
+       
     }
 }
--- Ваш БанкирЪ/MainPage.xaml.cs
+++ Ваш БанкирЪ/MainPage.xaml.cs
@@ -7,8 +7,11 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.IO;
+using System.Xml.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,29 +26,64 @@ namespace Ваш_БанкирЪ
 {
     public sealed partial class MainPage : Page
     {
-        
+        private bool CheckPasswordComplete(string login, string password)
+        {
+            XmlReader logPassReader = XmlReader.Create("G:\\Курсач\\Ваш БанкирЪ\\Ваш БанкирЪ\\bin\\x86\\Debug\\AppX\\LogPassDB.xml");
+            XmlDocument logPassDocument = new XmlDocument();
+            logPassDocument.Load(logPassReader);
+
+            MD5 md5hasher = MD5.Create();
+            byte[] data = md5hasher.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+                stringBuilder.Append(data[i].ToString("x2"));
+            string InputPasswordHash = stringBuilder.ToString();
+
+            XmlElement logPassRoot = logPassDocument.DocumentElement;
+            foreach (XmlNode User in logPassRoot)
+            {
+                if (User.Attributes.Count > 0)
+                {
+                    XmlNode loginNode = User.Attributes.GetNamedItem("login");
+                    if (loginNode.Value == login)
+                    {
+                        foreach (XmlNode UserChildNode in User.ChildNodes)
+                        {
+                            if (UserChildNode.Name == "passMD5" && UserChildNode.InnerText == InputPasswordHash)
+                                return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
+
         private void loginButton_Clicked(object sender, RoutedEventArgs e)
         {
-            XmlDocument xDoc = new XmlDocument();
-            xDoc.LoadXml();
+            string login = LoginTextBox.Text;
+            string password = PasswordBox.Password;
 
-            MD5 md5hasher = MD5.Create();
-            string Password = PasswordBox.Password;
-            byte[] data = md5hasher.ComputeHash(Encoding.UTF8.GetBytes(Password));
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < data.Length; i++)
-                stringBuilder.Append(data[i].ToString("x2"));
-            string passwordHash = stringBuilder.ToString();
-            
-
-            this.Frame.Navigate(typeof(MainMenuPage));
+            if (CheckPasswordComplete(login, password))
+            {
+                PasswordErrorFlyout.Hide();
+                Frame.Navigate(typeof(MainMenuPage));
+            }
+            else
+            {
+                PasswordErrorFlyout.ShowAt((Button)LoginButton);
+            }
         }
 
-        
+
+        private void PasswordErrorButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            PasswordErrorFlyout.Hide();
+        }
     }
 }
